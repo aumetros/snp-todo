3ecff903bac2cfdb061ea7b03a0ff8150bd5a415
aumetros <aum3tros@yandex.ru>
2023-04-30 20:23:25 +0300

    Рефакторинг приватных полей компонента Task

--- scripts/index.js
+++ scripts/index.js
@@ -98,10 +98,10 @@ function createNewTask(task) {
     editTask: (textContent, currentTextContent) => {
       const tasks = tasksLocalStorage.getArrayTasks();
       if (textContent === "" || textContent === currentTextContent) {
-        item._taskText.textContent = currentTextContent;
+        item.taskText.textContent = currentTextContent;
       } else if (tasksLocalStorage.isDublicateTask(textContent, tasks)) {
         alert("Такое задание у вас уже есть!");
-        item._taskText.textContent = currentTextContent;
+        item.taskText.textContent = currentTextContent;
       } else {
         tasksLocalStorage.editTask(textContent, currentTextContent);
       }
