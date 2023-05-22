a76a5723404eb519540dae52f4576f59e219af70
aumetros <aum3tros@yandex.ru>
2023-05-22 23:09:32 +0300

    BugFix: Реализован сабмит задачи при клике за приложением

--- scripts/components/Form.js
+++ scripts/components/Form.js
@@ -1,6 +1,7 @@
 export default class Form {
-  constructor(formSelector, { submitForm }) {
-    this._form = document.querySelector(formSelector);
+  constructor(formSelector, { submitForm }) {    
+    this._todo = document.querySelector(".todo");
+    this._form = this._todo.querySelector(formSelector);
     this._input = this._form.querySelector(".todo-form__input");
     this._submitForm = submitForm;
   }
@@ -16,10 +17,22 @@ export default class Form {
     this._form.reset();
   }
 
+  _handleClick = () => {
+    if (this._input.value !== "") {
+      this._submitForm(this._getInputValues());
+    }
+  };
+
   setEventListeners() {
     this._form.addEventListener("submit", (evt) => {
       evt.preventDefault();
       this._submitForm(this._getInputValues());
     });
+
+    this._todo.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
+
+    document.addEventListener("click", this._handleClick);
   }
 }
