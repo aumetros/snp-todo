8621cbdcc52a38e503dba02c366ac316098ba770
aumetros <aum3tros@yandex.ru>
2023-08-31 21:44:03 +0300

    Реализовано редактирование задачи

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 const app = document.querySelector(".todo");
 const formAddTask = app.querySelector(".todo-form");
 const inputAddTask = formAddTask.querySelector(".todo-form__input");
-const taskTemplate = document
-  .querySelector("#todo-list__item")
-  .content.querySelector(".todo-list__item");
+const taskTemplate = document.querySelector("#todo-list__item").content.querySelector(".todo-list__item");
 const todoList = app.querySelector(".todo-list");
 
 let tasks;
+let currentTask;
+let prevTaskText;
 
 localStorage.tasks
   ? (tasks = JSON.parse(localStorage.getItem("tasks")))
@@ -21,6 +21,15 @@ function addItemToTasks(task) {
   updateLocalStorage();
 }
 
+function editTaskInStorage(textContent, currentTextContent) {
+  tasks.forEach((task) => {
+    if (task.task === currentTextContent) {
+      task.task = textContent;
+    }
+  });
+  updateLocalStorage();
+}
+
 function deleteItemFromTasks(textContent) {
   tasks.forEach((task) => {
     if (task.task === textContent) {
@@ -32,14 +41,12 @@ function deleteItemFromTasks(textContent) {
 
 function handleDeleteTask(e) {
   const currentTask = e.target.closest(".todo-list__item");
-  const currentTaskText = currentTask.querySelector(
-    ".todo-list__item-text"
-  ).textContent;
+  const currentTaskText = currentTask.querySelector(".todo-list__item-text").textContent;
   deleteItemFromTasks(currentTaskText);
   currentTask.remove();
 }
 
-function toggleCompleteTask(e) {
+function toggleCompleteInTasks(e) {
   tasks.forEach((task) => {
     if (task.task === e.target.nextElementSibling.textContent) {
       task.complete = !task.complete;
@@ -50,7 +57,7 @@ function toggleCompleteTask(e) {
 
 function handleCompleteTask(e) {
   e.target.classList.toggle("todo-list__item-check_checked");
-  toggleCompleteTask(e);
+  toggleCompleteInTasks(e);
   // if (ls.getFilterFromStorage() !== "all") {
   //   setTimeout(() => {
   //     item.removeTaskElement();
@@ -58,6 +65,38 @@ function handleCompleteTask(e) {
   // }
   // counter.handleCounters(ls.getArrayTasks());
   // navBar.handleCommonButtons();
+}
+
+function isDublicateTask(taskText) {
+  const dublicate = tasks.some((todo) => {
+    return taskText === todo.task;
+  });
+  return dublicate;
+}
+
+function editTask () {
+  if (currentTask.textContent === "" || currentTask.textContent === prevTaskText) {
+    currentTask.textContent = prevTaskText;
+  } else if (isDublicateTask(currentTask.textContent)) {
+    alert("Такое задание у вас уже есть!");
+    currentTask.textContent = prevTaskText;
+  } else {
+    editTaskInStorage(currentTask.textContent, prevTaskText);
+  }
+}
+
+function confirmEditTask() {
+    editTask();
+    currentTask.contentEditable = false;
+    currentTask.removeEventListener("blur", confirmEditTask);
+}
+
+function handleEditTask(e) {
+  currentTask = e.target.previousElementSibling;
+  prevTaskText = currentTask.textContent;
+  currentTask.contentEditable = true;
+  currentTask.focus();
+  currentTask.addEventListener("blur", confirmEditTask);
 }
 
 function getInputValue() {
@@ -81,6 +120,7 @@ function createNewTask(task) {
   const taskDelete = newTask.querySelector(".todo-list__item-btn_type_delete");
   task.complete ? (taskCheck.classList.toggle("todo-list__item-check_checked")) : null;
   taskCheck.addEventListener("click", handleCompleteTask);
+  taskEdit.addEventListener("click", handleEditTask);
   taskDelete.addEventListener("click", handleDeleteTask);
   return newTask;
 }
